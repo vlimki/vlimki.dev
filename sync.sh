d7b2fd25325fdc5f34902b9e9de784e6660368bd
vlimki <vlimki@proton.me>
2024-06-08 23:06:43 +0300

    Automated commit for 08-06-2024

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 git add .
-git commit -m 'Automated commit for $(date +"%d-%m-%Y")'
+git commit -m "Automated commit for $(date +'%d-%m-%Y')"
 git push
 
 yarn build
-ssh $1 'sh "rm /var/www/html/* -rf"'
+ssh $1 'rm /var/www/html/* -rf'
 rsync build/ $1:/var/www/html/ -r
