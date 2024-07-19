7a4fe5ee7bc6e715c8e0a9744143ab4eb38c9f22
vlimki <vlimki@proton.me>
2024-07-20 01:34:25 +0300

    automated commit for 20-07-2024

---
+++ src/posts/log/day41.md
@@ -0,0 +1,16 @@
+---
+title: "Day 40 of Machine Learning"
+description: "Unproductive day today."
+published: true
+date: '2024-07-18'
+slug: 'day40'
+tags: ['log']
+post: false
+---
+<script>
+    import Image from '$lib/components/Image.svelte';
+</script>
+
+**Time spent**: 1h<br /> **Total**: 140h/10000h
+
+___
--- src/routes/writing/[slug]/+page.svelte
+++ src/routes/writing/[slug]/+page.svelte
@@ -8,25 +8,25 @@
 <svelte:head>
   <title>{data.frontmatter.title} • Juho Välimäki</title> 
   <meta name="description" content={data.frontmatter.description} />
-		<meta property="og:type" content="website" />
-	  <meta property="og:url" content="https://vlimki.dev/" />
-  	<meta property="og:title" content={`${data.frontmatter.title} • Juho Välimäki`} />
-	  <meta property="og:description" content={data.frontmatter.description} />
-  	<meta property="og:image" content="https://vlimki.dev/favicon.png" />
-	  <meta property="og:image:alt" content="Website logo" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://vlimki.dev/" />
+    <meta property="og:title" content={`${data.frontmatter.title} • Juho Välimäki`} />
+    <meta property="og:description" content={data.frontmatter.description} />
+    <meta property="og:image" content="https://vlimki.dev/favicon.png" />
+    <meta property="og:image:alt" content="Website logo" />
 
-  	<!-- Twitter -->
-  	<meta property="twitter:url" content="https://vlimki.dev/" />
-  	<meta property="twitter:title" content={`${data.frontmatter.title} • Juho Välimäki`} />
-	  <meta property="twitter:description" content={data.frontmatter.description} />
-	  <meta
-  	  property="twitter:image"
-    	content="https://vlimki.dev/favicon.png"
-	  />
-  	<meta
-    	property="twitter:image:alt"
-	    content="Website logo"
-  	/>
+    <!-- Twitter -->
+    <meta property="twitter:url" content="https://vlimki.dev/" />
+    <meta property="twitter:title" content={`${data.frontmatter.title} • Juho Välimäki`} />
+    <meta property="twitter:description" content={data.frontmatter.description} />
+    <meta
+      property="twitter:image"
+      content="https://vlimki.dev/favicon.png"
+    />
+    <meta
+      property="twitter:image:alt"
+      content="Website logo"
+    />
 </svelte:head> 
 
 <h1>{data.frontmatter.title}</h1>
@@ -34,23 +34,22 @@
   <code class="font-normal">{formatDate(new Date(data.frontmatter.date))}</code>
   <div class="mx-2 text-[#aaaaaa]">•</div>
   <div class="flex flex-row items-center">
-	  <code class="!text-md">[</code>
-  	{#each data.frontmatter.tags as tag}
-		<Tag text={tag} />
-  	{/each}
-  	<code>]</code>
+    <code class="!text-md">[</code>
+    {#each data.frontmatter.tags as tag}
+    <Tag text={tag} />
+    {/each}
+    <code>]</code>
   </div>
   <div class="mx-2 text-[#aaaaaa]">•</div>
   <code>{data.readTime} min read</code>
-
 </div>
 
 <article>
-	{@html data.content}
+  {@html data.content}
 </article>
 
 <style>
-	code {
-		font-size: 15px;
-	}
+  code {
+    font-size: 15px;
+  }
 </style>
