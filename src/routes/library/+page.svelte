db910cf15bec725c39add0b1119498eaec7dbf45
vlimki <vlimki@proton.me>
2024-08-13 19:16:05 +0300

    small updates

--- src/routes/library/+page.svelte
+++ src/routes/library/+page.svelte
@@ -9,10 +9,11 @@
 <div class="flex flex-row justify-center">
   <h1>Library</h1>
 </div>
-<p>I'm planning to write brief analyses about the books I read in this section. I'm doing it mainly to practice analyzing texts and to pursue my interest in philosophy.</p>
+<p>I'm planning to write brief analyses about the books I read in this section. I'm doing it mainly to practice analyzing texts and to pursue my interest in philosophy (although not all of the literature can be classified as strictly philosophy).</p>
 
 <h3>Reading</h3>
 <ol>
 	<li>Friedrich Nietzsche — The Antichrist
 	<li>Plato — The Republic
+	<li>Milton Friedman — Capitalism and Freedom
 </ol>
