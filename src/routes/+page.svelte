e6a9f8ab242bb488f6d43e895eadf995fd717ad9
vlimki <vlimki@proton.me>
2024-09-05 22:04:29 +0300

    update home page text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -33,7 +33,7 @@
 <div class="flex flex-col justify-between">
   <div class="flex flex-col">
     <h1 class="font-medium mb-2">Personal Website of Juho Välimäki</h1>
-		<p>On the road to becoming a machine learning engineer. Currently focusing heavily on mathematics and physics (mainly for school). Right now this website is mainly used for personal note-taking.</p>
+		<p>On the road to becoming a machine learning engineer. Currently focusing heavily on mathematics and physics. Right now this website is mainly used for personal note-taking.</p>
 		<p>What gets measured gets done.</p>
 		<h2 class="mb-1">Currently Reading (as of Sep 3)</h2>
 		<ul>
