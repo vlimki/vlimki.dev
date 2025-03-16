08534df7f38e8f8c2881c7ba89eb457b210293bb
vlimki <vlimki@proton.me>
2025-03-17 00:01:34 +0200

    update font sizes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -32,10 +32,10 @@
 	}
 </script>
 
-<div class="max-w-screen overflow-x-hidden flex flex-col items-center justify-center">
+<div class="max-w-screen overflow-x-hidden flex flex-col items-center justify-center dark:imgbg">
 	<div class="w-full md:w-2/3 xl:w-[55%] 2xl:w-[41%] h-full pb-10 px-7">
 		<nav class="flex flex-row w-full items-center justify-between mt-5 text-md">
-				<a href="/"><b class="md:block hidden m-0 p-0"><code class="!text-[#444444] dark:!text-[#cccccc] text-sm hidden sm:block">vlimki.dev</code></b></a>
+				<a href="/"><b class="md:block hidden m-0 p-0"><code class="!text-[#444444] dark:!text-[#cccccc] !text-[14px] hidden sm:block">vlimki.dev</code></b></a>
 			<div class="flex flex-row items-center">
 				<a href="/"><code class={path == "/" ? "font-semibold text-[#444444] dark:text-[#cccccc]" : ""}>Home</code></a>
 				<p class="!my-0 text-xxs text-[#555555]">•</p>
@@ -43,9 +43,9 @@
 			</div>
 			<div class="sm:min-w-[92px] flex sm:max-w-[92px] !my-0 pr-3 justify-end items-end">
 				{#if darkMode}
-					<img on:click={handleDarkMode} class="cursor-pointer h-5 w-5" src="/images/darkmode.png" />
+					<img on:click={handleDarkMode} class="cursor-pointer h-4 w-4" src="/images/darkmode.png" />
 				{:else}
-					<img on:click={handleDarkMode} class="cursor-pointer h-5 w-5" src="/images/lightmode.png" />
+					<img on:click={handleDarkMode} class="cursor-pointer h-4 w-4" src="/images/lightmode.png" />
 				{/if}
 			</div>
 		</nav>
@@ -55,23 +55,23 @@
 		<div class="flex ftr flex-row items-center justify-center sm:justify-between px-3">
 		<div class="flex flex-row links text-[#0471d7] dark:text-[#0b9ddd] items-center justify-center">
 				{#if darkMode}
-					<a target="_blank" rel="noreferrer" href="https://github.com/vlimki" class="mr-5 !text-[16px] w-5 h-5 sm:!text-base"><img src="/images/github-light.png" /></a>
-					<a href="mailto:root@vlimki.dev" class="mx-5 !text-[16px] sm:!text-base w-5 h-5 mt-[6px]"><img src="/images/email-light.png" /></a>
-					<a href="https://x.com/vl1mki" rel="noreferrer" target="_blank" class="mx-5 mb-1 !text-[16px] sm:!text-base w-5 h-5 mt-[6px]"><img src="/images/x-light.png" /></a>
+					<a target="_blank" rel="noreferrer" href="https://github.com/vlimki" class="mr-5 !text-[16px] w-4 h-4 sm:!text-base"><img src="/images/github-light.png" /></a>
+					<a href="mailto:root@vlimki.dev" class="mx-5 !text-[16px] sm:!text-base w-4 h-4 mt-[6px]"><img src="/images/email-light.png" /></a>
+					<a href="https://x.com/vl1mki" rel="noreferrer" target="_blank" class="mx-5 mb-1 !text-[16px] sm:!text-base w-4 h-4 mt-[6px]"><img src="/images/x-light.png" /></a>
 				{:else}
-					<a target="_blank" rel="noreferrer" href="https://github.com/vlimki" class="mr-5 !text-[16px] w-5 h-5 sm:!text-base"><img src="/images/github.png" /></a>
-					<a href="mailto:root@vlimki.dev" class="mx-5 !text-[16px] sm:!text-base w-5 h-5 mt-[6px]"><img src="/images/email.png" /></a>
-					<a href="https://x.com/vl1mki" target="_blank" rel="noreferrer" class="mb-1 mx-5 !text-[16px] sm:!text-base w-5 h-5 mt-[6px]"><img src="/images/x.png" /></a>
+					<a target="_blank" rel="noreferrer" href="https://github.com/vlimki" class="mr-5 !text-[16px] w-4 h-4 sm:!text-base"><img src="/images/github.png" /></a>
+					<a href="mailto:root@vlimki.dev" class="mx-5 !text-[16px] sm:!text-base w-4 h-4 mt-[6px]"><img src="/images/email.png" /></a>
+					<a href="https://x.com/vl1mki" target="_blank" rel="noreferrer" class="mb-1 mx-5 !text-[16px] sm:!text-base w-4 h-4 mt-[6px]"><img src="/images/x.png" /></a>
 				{/if}
 		</div>
-		<code class="text-xxs text-[#aaaaaa] hidden mt-2 sm:block"><a class="font-semibold text-[#888888] footer-link" href="/">vlimki.dev</a> | Based in 🇫🇮</code>
+		<code class="text-[13px] text-[#aaaaaa] hidden mt-2 sm:block"><a class="font-semibold text-[#888888] footer-link" href="/">vlimki.dev</a> | Based in 🇫🇮</code>
 		</div>
 	</div>
 </div>
 
 <style lang="postcss">
 	.links a {
-		font-size: 14px;
+		font-size: 10px;
 	}
 
 	.links a {
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import PageBreak from '$lib/components/PageBreak.svelte';
+</script>
+
+<svelte:head>
+	<title>About - Juho Välimäki</title>
+	<meta name="description" content="I'm an aspiring and ambition-driven machine learning engineer." />
+</svelte:head> 
+
+<div class="flex flex-row ">
+	<div class="mr-5">
+		<img class="aspect-square rounded-full object-cover" alt="image of Juho" src="/images/pfp.png" />
+	</div>
+	<div class="sm:ml-10">
+		<h1 class="">About</h1>
+		<p>Hey - I'm Juho Välimäki, and I aspire to be a machine learning engineer. At first I got into ML because I saw it as the only sensible route for the future (due to product advancements like ChatGPT), but soon enough it completely replaced my interest for web/software engineering.</p>
+		<p class="!my-0" >My interests include:</p>
+		<ul>
+			<li>machine learning + mathematics</li>
+			<li>self-improvement</li>
+			<li>kickboxing + weightlifting</li>
+			<li>playing the guitar + music production.</li>
+		</ul>
+
+		<p>For anyone interested in connecting - don't hesitate to send me an email at <a href="mailto:root@vlimki.dev">root@vlimki.dev</a>.</p>
+	</div>
+</div>
+<style>
+	img {
+
+		border-radius: 1000px;
+    	aspect-ratio: 1;
+    	object-fit: cover;
+	}
+</style>
