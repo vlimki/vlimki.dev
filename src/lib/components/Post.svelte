0d4fd7c658d6ff03aa3154d23c4af997594928af
vlimki <vlimki@proton.me>
2025-05-09 23:12:18 +0300

    UI updates

--- src/lib/components/Post.svelte
+++ src/lib/components/Post.svelte
@@ -6,7 +6,7 @@
 
 <div class="mb-6">
 		<a href={`/writing/${post.slug}`} class="flex flex-col hover:no-underline cursor-pointer">
-	    <h3 class="text-[#252525] dark:text-[#fcfcfc]">{post.title}</h3>
+	    <h3 class="text-[#252525] !font-normal dark:text-[#fcfcfc]">{post.title}</h3>
   	  <p class="!my-0 !mb-1 text-[#666666] dark:text-[#999999] text-[14px] sm:text-[15px] leading-5 sm:leading-[1.4rem] md:text-sm">{post.description}</p>
 		</a>
     <div class="flex flex-row items-center flex-wrap wrap">
